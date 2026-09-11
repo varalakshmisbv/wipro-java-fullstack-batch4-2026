57f7c3289fa64ef8b28e047335c52bdabd831970
varalakshmisbv <varalakshmisbv@gmail.com>
2026-09-11 12:02:16 +0530

    Add files via upload
    added sample java file using maven build tool and netbeans IDE

---
+++ Addition.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author student-user
+ */
+public class Addition {
+    public static void main(String[] args) {
+    int a =10 ;
+    int b = 20 ;
+    int sum = a+b;
+    System.out.println(sum);    
+    }
+    
+    
+}
